bbed8ac2880a3dafbb42c8f820894863c4ad5e64
Mario Blažek <blazek.mario@gmail.com>
2017-07-26 09:58:58 +0200

    Exceptions updated

--- bundle/Exception/PropertyNotFoundException.php
+++ bundle/Exception/PropertyNotFoundException.php
@@ -10,15 +10,11 @@ class PropertyNotFoundException extends Exception
      * Generates: Property '{$propertyName}' not found.
      *
      * @param string $propertyName
-     * @param string|null $className Optionally to specify class in abstract/parent classes
-     * @param \Exception|null $previous
+     * @param string $className
+     * @param Exception|null $previous
      */
-    public function __construct($propertyName, $className = null, Exception $previous = null)
+    public function __construct($propertyName, $className, Exception $previous = null)
     {
-        if ($className === null) {
-            parent::__construct("Property '{$propertyName}' not found", 0, $previous);
-        } else {
-            parent::__construct("Property '{$propertyName}' not found on class '{$className}'", 0, $previous);
-        }
+        parent::__construct("Property '{$propertyName}' not found on class '{$className}'", 0, $previous);
     }
 }
--- bundle/Exception/PropertyReadOnlyException.php
+++ bundle/Exception/PropertyReadOnlyException.php
@@ -10,15 +10,11 @@ class PropertyReadOnlyException extends Exception
      * Generates: Property '{$propertyName}' is readonly[ on class '{$className}'].
      *
      * @param string $propertyName
-     * @param string|null $className Optionally to specify class in abstract/parent classes
-     * @param \Exception|null $previous
+     * @param string $className
+     * @param Exception|null $previous
      */
     public function __construct($propertyName, $className = null, Exception $previous = null)
     {
-        if ($className === null) {
-            parent::__construct("Property '{$propertyName}' is readonly", 0, $previous);
-        } else {
-            parent::__construct("Property '{$propertyName}' is readonly on class '{$className}'", 0, $previous);
-        }
+        parent::__construct("Property '{$propertyName}' is readonly on class '{$className}'", 0, $previous);
     }
 }
