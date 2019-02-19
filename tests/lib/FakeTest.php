f5fbb851689cbb2d311a8f7272290d4cc7e3a320
Mario Blažek <blazek.mario@gmail.com>
2019-02-19 13:21:17 +0100

    Refactor test

---
+++ tests/lib/FakeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Netgen\InformationCollection\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FakeTest extends TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
