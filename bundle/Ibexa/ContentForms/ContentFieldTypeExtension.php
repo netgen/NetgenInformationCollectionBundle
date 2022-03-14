9dd5bbd6cd5696f40886e21f10129fd583c1738d
Edi Modrić <edi.modric@gmail.com>
2022-03-14 17:06:47 +0100

    Disable isRequired check for info collector fields

---
+++ bundle/Ibexa/ContentForms/ContentFieldTypeExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\InformationCollectionBundle\Ibexa\ContentForms;
+
+use Ibexa\ContentForms\Form\Type\Content\ContentFieldType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContentFieldTypeExtension extends AbstractTypeExtension
+{
+    public function getExtendedType(): string
+    {
+        return ContentFieldType::class;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ContentFieldType::class];
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $fieldIdentifier = $builder->getName();
+
+        /** @var \Ibexa\ContentForms\Data\Content\ContentUpdateData $updateStruct */
+        $updateStruct = $options['contentUpdateStruct'];
+
+        $fieldDefinition = $updateStruct->fieldsData[$fieldIdentifier]->fieldDefinition;
+        if ($fieldDefinition->isInfoCollector) {
+            $builder->setRequired(false);
+
+            (function () {
+                $this->isRequired = false;
+            })->call($fieldDefinition);
+        }
+    }
+}
