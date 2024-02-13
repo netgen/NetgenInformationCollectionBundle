349d7fe0a1c337e2719b5b0e997fcaac77f3006f
Miljenko Muha <miljenko@netgen.io>
2024-02-13 13:08:06 +0100

    FINAWEBSUP-11 property period made private

--- bundle/Command/DeleteOldCollectedInfoCommand.php
+++ bundle/Command/DeleteOldCollectedInfoCommand.php
@@ -37,7 +37,7 @@ class DeleteOldCollectedInfoCommand extends Command
     /**
      * @var DateInterval
      */
-    protected $period;
+    private $period;
 
     /**
      * @var InformationCollection
